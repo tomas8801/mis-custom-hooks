67b3316253329cc6a26dfa1ecaa8de78efb45e03
Tomas Marsili <tomasmarsili.contacto@gmail.com>
2021-09-02 00:39:51 -0300

    Primer commit

---
+++ useFetch.js
@@ -0,0 +1,50 @@
+import { useEffect, useState, useRef } from "react"
+
+export const useFetch = ( url ) => {
+
+    const isMounted = useRef(true)
+    const [state, setState] = useState({ data: null, loading: true, error: null});
+    
+    useEffect(() => {
+        
+        return () => {
+            isMounted.current = false;
+        }
+    }, [])
+
+
+    useEffect(() => {
+
+        setState({ data: null, loading: true, error: null})
+
+
+    
+            fetch( url )
+            .then( res =>  res.json() )
+            .then( data => {
+                if(isMounted.current) {
+
+                    setState({
+                        loading: false,
+                        error: null,
+                        data
+    
+                    })
+                } else {
+                    console.log('setState no se llamó')
+                }
+                
+            })
+            .catch(() => {
+                setState({
+                    data: null,
+                    loading: false,
+                    error: 'No se pudo cargar la info'
+                })
+            })
+        
+
+    }, [ url ])
+
+    return state;
+}
